a541894b763b06fa1972421b157cba8896623fb9
tnqls7742 <tnqls7742@douzone.com>
2022-09-13 14:10:10 +0900

    chapter02-section02-exam01

---
+++ src/chapter02/sec02/Exam01.java
@@ -0,0 +1,10 @@
+package chapter02.sec02;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("chapter02.sec02");
+	}
+
+}
