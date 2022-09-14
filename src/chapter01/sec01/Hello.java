dc19f2c237defc47c4fce53330c3497dc404fb44
bingSoo99 <90229179+soobin10236@users.noreply.github.com>
2022-09-14 09:12:56 +0900

    Update Hello.java

--- src/chapter01/sec01/Hello.java
+++ src/chapter01/sec01/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		//pull test2
+		
+		System.out.println("Hello Java");
 	}
 
 }
