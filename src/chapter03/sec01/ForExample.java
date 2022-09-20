1f1674619be29e5e4f43646dba377eb6cdc4e462
tnqls7742 <tnqls7742@douzone.com>
2022-09-20 17:58:28 +0900

    chap03 - For_Example

---
+++ src/chapter03/sec01/ForExample.java
@@ -0,0 +1,35 @@
+package chapter03.sec01;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println((i + 1) + "번째 반복");
+//		}
+		
+		
+//		for (int i = 0; i < 10; i = i + 2) {
+//			System.out.println(i);
+//		}
+		
+//		for (int i = 10; i > 0; i--) {
+//			System.out.println(i);
+//		}
+		
+		
+//		int i = 0;
+//		
+//		for (; ;) {
+//			if(i < 10) {
+//				System.out.println(i);
+//			} else {
+//				break;
+//			}
+//			i += 1;
+//		}
+		
+		
+	}
+
+}
