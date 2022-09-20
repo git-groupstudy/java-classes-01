e93992f147bae8b696ef6722e63c7d3b38a21d5c
soobin lee <tnqls7742@gmail.com>
2022-09-20 23:13:00 +0900

    220920 - 챕터03 예제 작성

---
+++ src/chapter03/sec01/IfSwitchExample.java
@@ -0,0 +1,70 @@
+package chapter03.sec01;
+
+public class IfSwitchExample {
+public static void main(String[] args) {
+		
+		//1. if문 기초
+//		if(true) {
+//			System.out.println("항상 실행");
+//		}
+//		
+//		if(false) {
+//			System.out.println("항상 실행 안됨");
+//		}
+		
+		//2. if, if - else
+		
+//		int a = 10;
+//		int b = 20;
+//		
+//		if(a > b) {
+//			System.out.println("a가 b보다 큼");
+//		}
+//		
+//		if(a > b) {
+//			System.out.println("a가 b보다 큼");
+//		} else {
+//			System.out.println("a가 b보다 작음");
+//		}
+		
+		//3. if - else if - else
+//		int x = 10;
+//		int y = 10;
+//		
+//		if(x > y) {
+//			System.out.println("x가 y보다 큼");
+//		} else if(x < y) {
+//			System.out.println("x가 y보다 작음");
+//		} else {
+//			System.out.println("x와 y가 같음");
+//		}
+		
+		
+		//4. switch문
+//		String key = "Hello";
+//		
+//		switch (key) {
+//		case "Hello": System.out.println("안녕");
+//			
+//			break;
+//		
+//		case "Hello2" : System.out.println("안녕2");
+//		
+//			break;
+//
+//		default:
+//			System.out.println("안녕못해");
+//			break;
+//		}
+		
+		//5. switch - if
+//		if(key.equals("Hello")) {
+//			System.out.println("안녕");
+//		} else if(key.equals("Hello2")) {
+//			System.out.println("안녕2");
+//		} else {
+//			System.out.println("안녕못해");
+//		}
+		
+	}
+}
---
+++ src/chapter03/sec01/Stars.java
@@ -0,0 +1,77 @@
+package chapter03.sec01;
+
+public class Stars {
+	public static void main(String[] args) {
+		//1. 5 * 5 2차원 배열 알아보기
+//		for(int i=0; i<5; i++) {
+//			for(int j=0; j<5; j++) {
+//				System.out.println("[i : " + i + "], [j : " + j + "]");
+//			}
+//		}
+		
+		
+		//2. 5 * 5 2차원배열 출력
+//		for (int i = 0; i < 5; i++) {
+//			for (int j = 0; j < 5; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+		
+		
+		//3. 삼각형 출력
+//		for (int i = 0; i < 5; i++) {
+//			for (int j = 0; j < 5; j++) {
+//				if(i >= j) {
+//					System.out.print("*");
+//				}
+//			}
+//			System.out.println("");
+//		}
+		
+		//4. 삼각형  반대로 출력
+//		for (int i = 0; i < 5; i++) {
+//			for (int j = 4; j >= 0; j--) {
+//				if(i >= j) {
+//					System.out.print("*");					
+//				} else {
+//					System.out.print(" ");
+//				}
+//			}
+//			System.out.println("");
+//		}
+		
+		//5. 피라미드 출력
+//		for (int i = 0; i < 5; i++) {
+//			
+//			for (int j = 4; j >= 0; j--) {
+//				if(i >= j) {
+//					System.out.print("*");					
+//				} else {
+//					System.out.print(" ");
+//				}
+//			}
+//			
+//			for (int j = 0; j < 5; j++) {
+//				if(i >= j) {
+//					System.out.print("*");
+//				}
+//			}
+//			System.out.println("");
+//		}
+		
+//		for(int i = 0; i < 5; i++){
+//			
+//			for(int j = 0; j < (4 - i); j++){
+//				System.out.print(" "); 
+//			}
+//			
+//			for(int j = 0; j < (2 * i + 1); j++){
+//				System.out.print("*"); 
+//			}
+//			
+//			System.out.println("");
+//		}
+		
+	}
+}
